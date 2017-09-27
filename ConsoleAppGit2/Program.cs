576c8344862f8bdff667446ea462b0f2db277917
Harn <1025280231@qq.com>
2017-09-27 15:43:59 +0800

    创建了a/b

--- ConsoleAppGit2/Program.cs
+++ ConsoleAppGit2/Program.cs
@@ -11,9 +11,10 @@ namespace ConsoleAppGit2
         static void Main(string[] args)
         {
             Console.WriteLine("666");
+            int a = 3;
+            int b = 5;
+            Console.WriteLine(a / 5);
             Console.ReadLine();
-            Console.WriteLine("777");
-            Console.WriteLine("888");
         }
     }
 }
