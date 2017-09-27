2176c37193b9a764306132eaaf44c52bdbb5426a
Harn <1025280231@qq.com>
2017-09-27 15:35:35 +0800

    添加项目文件。

---
+++ ConsoleAppGit2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGit2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("666");
+            Console.ReadLine();
+        }
+    }
+}
